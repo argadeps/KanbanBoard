7c1d2dcc1e45ee0b43dfe6757c4638f16d8da96c
[Pral Argade] <[pralargade@outlook.com]>
2025-02-20 00:05:14 -0500

    changes made for ssl errors

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,23 +1,32 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { Sequelize } from 'sequelize';
+import { Options, Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        }
+const sequelize_options: Options = {
+  host: "localhost",
+  dialect: "postgres",
+  dialectOptions: {
+    decimalNumbers: true,
+    ssl: {
+        require: true,
+        rejectUnauthorized: false, // Important for self-signed certificates
       },
-    });
+  },
+};
+
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL,sequelize_options)
+  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD,{
+    host: 'localhost',
+    dialect: 'postgres',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  } 
+);
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
